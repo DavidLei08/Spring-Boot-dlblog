99521c474c6445654e8c58c52b8a7427e77f8dda
DavidLei08 <2686849744@qq.com>
2020-05-23 13:41:20 +0800

    spring boot admin 监控服务集成

---
+++ admin-server/src/test/java/org/thanos/admin/server/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thanos.admin.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
